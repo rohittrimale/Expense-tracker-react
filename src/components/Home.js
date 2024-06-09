0ebee1b4174f7ddde8fc131045be5ba63283b49a
rohittrimale <rohittrimalle@gmail.com>
2024-06-10 02:06:52 +0530

    when user come on  home page without login then reder to login page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { user } = useContext(UserContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [navigate, user]);
+
   return (
     <div className="text-center bg-orange-400 py-2 mt-5">
-      {" "}
       Welcome to Expense Tracking{" "}
     </div>
   );
